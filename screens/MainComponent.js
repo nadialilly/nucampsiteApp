e3fe33f8ce49c217d0c855d7041462db5f264f01
Nadia Chester <nadiachester3@gmail.com>
2023-07-10 12:13:39 -0400

    added header in drawer

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,14 +1,15 @@
 
-import { View, Platform , StyleSheet} from 'react-native';
+import { View, Platform , StyleSheet, Image, Text} from 'react-native';
 import Constants from 'expo-constants';
 import DirectoryScreen from './DirectoryScreen';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements';
+import logo from '../assets/images/logo.png';
 
 
 const Drawer = createDrawerNavigator();
@@ -120,7 +121,22 @@ const DirectoryNavigator = () => {
                     })} />
             </Stack.Navigator>
     )
-}
+};
+
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{flex: 1}}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{flex: 2}}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View>
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontweight: 'bold'}} />
+    </DrawerContentScrollView>
+    
+)
 const Main = () => {
     
 
@@ -128,6 +144,7 @@ const Main = () => {
         <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
                 <Drawer.Navigator
                     initialRouteName='Home'
+                    drawerContent={CustomDrawerContent}
                     drawerStyle={{ backgroundColor: '#CEC8FF'}}
                 >
                     <Drawer.Screen
@@ -201,6 +218,25 @@ const Main = () => {
 };
 
 const styles = StyleSheet.create({
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
+    
     stackIcon: {
         marginLeft: 10,
         color: '#fff',
