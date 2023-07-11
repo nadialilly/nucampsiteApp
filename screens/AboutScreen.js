812c6237081744dee0a900955282981333d5eaea
Nadia Chester <nadiachester3@gmail.com>
2023-07-11 10:16:36 -0400

    adding favorite in redux

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,6 +3,7 @@ import { ScrollView } from "react-native-gesture-handler";
 import { Avatar, ListItem, Card, Text } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from '../components/loadingComponent';
 
 
 const Mission = () => {
@@ -20,25 +21,51 @@ const Mission = () => {
 const AboutScreen = () => {
    const partners = useSelector((state) => state.partners);
 
+   if (partners.isLoading) {
+        return (
+            <ScrollView>
+                    <Mission />
+                    <Card>
+                        <Card.Title>Community Partners</Card.Title>
+                        <Card.Divider />
+                        <Loading />
+                    </Card>
+                    </ScrollView>
+        );
+    }
+
+    if (partners.errMess) {
+        return(
+            <ScrollView>
+                    <Mission />
+                    <Card>
+                        <Card.Title>Community Partners</Card.Title>
+                        <Card.Divider />
+                       <Text>{partners.errMess}</Text>
+                    </Card>
+                    </ScrollView>
+        )
+    };
+   
     return(
         <ScrollView>
-           <Mission />
-           <Card>
-            <Card.Title>Community Partners</Card.Title>
-            <Card.Divider />
-            {partners.partnersArray.map((partner) => {
-                return (
-                    <ListItem key={partner.id}>
-                        <Avatar rounded source={{uri: baseUrl + partner.image}}/>
-                        <ListItem.Content>
-                            <ListItem.Title>{partner.name}</ListItem.Title>
-                            <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
-                        </ListItem.Content>
-                    </ListItem>
-                )
-            })}
-           </Card>
-        </ScrollView>
+                    <Mission />
+                    <Card>
+                        <Card.Title>Community Partners</Card.Title>
+                        <Card.Divider />
+                        {partners.partnersArray.map((partner) => {
+                            return (
+                                <ListItem key={partner.id}>
+                                    <Avatar rounded source={{uri: baseUrl + partner.image}}/>
+                                    <ListItem.Content>
+                                        <ListItem.Title>{partner.name}</ListItem.Title>
+                                        <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                                    </ListItem.Content>
+                                </ListItem>
+                            )
+                        })}
+                    </Card>
+                    </ScrollView>
     )
 };
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,12 +3,24 @@ import { Text, View, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-//import { CAMPSITES } from '../shared/campsites';
-//import { PROMOTIONS } from '../shared/promotions';
-//import { PARTNERS } from '../shared/partners';
+import Loading from '../components/loadingComponent';
 
 
-const FeaturedItem = ({item}) => {
+const FeaturedItem = (props) => {
+    const {item } = props;
+
+    if (props.isLoading) {
+        return <Loading />
+    }
+
+    if (props.errMess) {
+        return(
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
+
     if(item) {
         return(
             <Card containerStyle={{ padding: 0 }}>
@@ -45,9 +57,21 @@ const HomeScreen = () => {
 
     return(
         <ScrollView>
-            <FeaturedItem item={featCampsite} />
-            <FeaturedItem item={featPromotion} />
-            <FeaturedItem item={featPartner} />
+            <FeaturedItem 
+            item={featCampsite}
+            isLoading={campsites.isLoading}
+            errMess={campsites.errMess} />
+
+            <FeaturedItem 
+            item={featPromotion}
+            isLoading={promotions.isLoading}
+            errMess={promotions.errMess} />
+
+            <FeaturedItem 
+            item={featPartner}
+            isLoading={partners.isLoading}
+            errMess={partners.errMess} />
+
         </ScrollView>
     );
 };
